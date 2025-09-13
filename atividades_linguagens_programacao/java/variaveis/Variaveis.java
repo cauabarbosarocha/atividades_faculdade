1bf8da1e8b4253cf42e1ba94762ae89fc8c5dadd
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-09-13 15:09:49 -0300

    Declaração de variaveis

---
+++ atividades_linguagens_programacao/java/variaveis/Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        // Declarando e inicializando variáveis
+        int idade = 19;
+        double altura = 1.70;
+        char genero = 'M';
+        boolean estudante = true;
+
+        // Exibindo os valores das variáveis
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Estudante: " + estudante);
+    }
+}
