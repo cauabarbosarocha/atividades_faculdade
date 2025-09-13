4e90497758167342ab12e3cb4a5ac8456668eef2
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-09-13 16:06:15 -0300

    Leitura de dados

---
+++ atividades_linguagens_programacao/java/entrada_de_dados/LeituraDados.java
@@ -0,0 +1,18 @@
+package atividades_linguagens_programacao.java.entrada_de_dados;
+import java.util.Scanner;
+
+public class LeituraDados {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite sue idade: ");
+        int idade = sc.nextInt();
+
+        System.out.println(nome + " tem " + idade + " anos.");
+
+        sc.close();
+    }
+}
