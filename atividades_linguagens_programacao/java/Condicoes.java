b0c7d6cc76b3b318d2ba1a069792368c8fc37978
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-10-04 11:11:39 -0300

    Começo do estudo de condições
    Nota-se que ambas as linguagens desempenham o mesmo papel, porém com uma grande diferença na sintaxe.

---
+++ atividades_linguagens_programacao/java/Condicoes.java
@@ -0,0 +1,24 @@
+package atividades_linguagens_programacao.java;
+
+import java.util.Scanner;
+
+public class Condicoes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int idade;
+
+        // Solicitação da idade do usuário
+        System.out.print("Digite sua idade: ");
+        idade = scanner.nextInt();
+        
+        // Validação da idade do usuário
+        if (idade >= 18) {
+            System.out.print("Acesso permitido.");
+        } else {
+            System.out.print("Acesso negado!");
+        }
+        
+         scanner.close();
+
+    }
+}
