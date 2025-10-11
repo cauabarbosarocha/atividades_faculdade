3bda3413fc088ac63348b3aefdd0e356f5db8c31
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-10-11 10:14:31 -0300

    Melhoria inicial do código

--- atividades_linguagens_programacao/java/exercicios/Viagem.java
+++ atividades_linguagens_programacao/java/exercicios/Viagem.java
@@ -18,6 +18,8 @@ public class Viagem {
         // Verifica as condições para o destino
         if (ensolarado && fimDeSemana) {
             System.out.print("Vamos para a praia e aproveitar o Sol.");
+        } else {
+            System.out.print("Vamos para o parque e andar por lá.");
         }
 
         scanner.close();
