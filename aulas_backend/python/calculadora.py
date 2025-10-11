b7b3c4cfbd89f5c3a5b8d1b0f5f6d136ae6e19c3
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-10-11 11:58:22 -0300

    Havia esquecido de mostrar o resultado das operações

--- aulas_backend/python/calculadora.py
+++ aulas_backend/python/calculadora.py
@@ -16,13 +16,17 @@ resultado = 0
 match operador:
     case 1:
         resultado = numero_1 + numero_2
+        print(f"O resutado da soma é: {resultado}")
     case 2:
         resultado = numero_1 - numero_2
+        print(f"O reultado da subtração é: {resultado}")
     case 3:
         resultado = numero_1 * numero_2
+        print(f"o Resultado da multiplicação é: {resultado}")
     case 4:
         if numero_2 != 0:
             resultado = numero_1 / numero_2
+            print(f"O resultado da Divisão é: {resultado}")
         else:
             print("[ERRO] Impossível dividir por zero")
     case _:
