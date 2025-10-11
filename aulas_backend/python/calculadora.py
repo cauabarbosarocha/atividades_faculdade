e8fd57b0f351414cb00a040fb991c4c196e463ff
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-10-11 11:53:48 -0300

    Calculadora em python para aprender o uso de macth case

---
+++ aulas_backend/python/calculadora.py
@@ -0,0 +1,29 @@
+# Variáveis
+operador = int(input(
+    "Bem-vindo à calculadora!" \
+    " \nPor favor, escolha a operação:" \
+    " \n1 - Soma" \
+    " \n2 - Subtração" \
+    " \n3 - Multiplicação" \
+    " \n4 - Divisão" \
+    " \nOperador escolhido: "
+    ))
+numero_1 = float(input("Digite o primeiro número: "))
+numero_2 = float(input("Digite o segundo número: "))
+
+# Processamento
+resultado = 0
+match operador:
+    case 1:
+        resultado = numero_1 + numero_2
+    case 2:
+        resultado = numero_1 - numero_2
+    case 3:
+        resultado = numero_1 * numero_2
+    case 4:
+        if numero_2 != 0:
+            resultado = numero_1 / numero_2
+        else:
+            print("[ERRO] Impossível dividir por zero")
+    case _:
+        print("[ERRO] Operador inválido")
