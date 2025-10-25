9d692412ea1b9408d6fccc432f66be11451822e3
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-10-25 14:06:57 -0300

    Melhoria no sistema de café utilizando o while

--- aulas_backend/java/PedidoCafe.java
+++ aulas_backend/java/PedidoCafe.java
@@ -5,58 +5,70 @@ import java.util.Scanner;
 public class PedidoCafe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         // Variáveis
         String tamanhoCafe;
         int tipoCafe;
         double valorCafe = 0.0;
-
-        // Recebe o tamanho do café
-        System.out.print(
-                "Bem-vindo à cafeteria \nEscolha o tamanho do café: \n [P] Pequeno (R$ 2,50) \n [M] Médio (R$ 3,00) \n [G] Grande (R$ 3,50) \nTamanho escolhido: ");
-        tamanhoCafe = scanner.nextLine().toUpperCase();
-
-        // Verifica o tamanho do café e atribui ao valor
-        switch (tamanhoCafe) {
-            case "P":
-                valorCafe += 2.50;
-                break;
-            case "M":
-                valorCafe += 3.00;
-                break;
-            case "G":
-                valorCafe += 3.50;
-                break;
-            default:
-                System.out.println("[ERRO] Tamanho inválido.");
+        double valorTotal = 0.0;
+        int quantidadeCafe = 0;
+        boolean continuarPedido = true;
+        // Laço para realizar o pedido
+        while (continuarPedido) {
+            // Recebe o tamanho do café
+            System.out.print(
+                    "Bem-vindo à cafeteria Java! \nDigite [F] para finalizar \nEscolha o tamanho do café: \n [P] Pequeno (R$ 2,50) \n [M] Médio (R$ 3,00) \n [G] Grande (R$ 3,50) \nOpção escolhida: ");
+            tamanhoCafe = scanner.nextLine().toUpperCase();
+            // Verifica se o cliente deseja finalizar o pedido
+            if (tamanhoCafe.equals("F")) {
+                continuarPedido = false;
+                System.out.print("Pedido finalizado. Obrigado por escolher a cafeteria Java!");
                 scanner.close();
-                return;
+            } else {
+                // Verifica o tamanho do café e atribui ao valor
+                switch (tamanhoCafe) {
+                    case "P":
+                        valorCafe += 2.50;
+                        break;
+                    case "M":
+                        valorCafe += 3.00;
+                        break;
+                    case "G":
+                        valorCafe += 3.50;
+                        break;
+                    default:
+                        System.out.println("[ERRO] Tamanho inválido.");
+                        continue;
+                }
+                // Recebe o tipo do café
+                System.out.print(
+                        "Escolha o tipo do café: \n [1] Expresso (R$ 1,50) \n [2] Cappuccino (R$ 2,00) \n [3] Latte (R$ 2,50) \nTipo escolhido: ");
+                tipoCafe = scanner.nextInt();
+                // Verifica o tipo de café e atribui ao valor
+                switch (tipoCafe) {
+                    case 1:
+                        valorCafe += 1.50;
+                        break;
+                    case 2:
+                        valorCafe += 2.00;
+                        break;
+                    case 3:
+                        valorCafe += 2.50;
+                        break;
+                    default:
+                        System.out.println("[ERRO] Tipo inválido.");
+                        continue;
+                }
+                // Adiciona a quantidade de cafés e o valor total
+                valorTotal += valorCafe;
+                quantidadeCafe++;
+                System.out.println("Café adicionado ao pedido. Valor atual: R$" + valorTotal);
+                // Limpa o buffer do scanner
+                scanner.nextLine();
+            }
         }
-
-        // Recebe o tipo do café
-        System.out.print(
-                "Escolha o tipo do café: \n [1] Expresso (R$ 1,50) \n [2] Cappuccino (R$ 2,00) \n [3] Latte (R$ 2,50) \nTipo escolhido: ");
-        tipoCafe = scanner.nextInt();
-
-        // Verifica o tipo de café e atribui ao valor
-        switch (tipoCafe) {
-            case 1:
-                valorCafe += 1.50;
-                break;
-            case 2:
-                valorCafe += 2.00;
-                break;
-            case 3:
-                valorCafe += 2.50;
-                break;
-            default:
-                System.out.println("[ERRO] Tipo inválido.");
-                scanner.close();
-                return;
-        }
-
-        // Exbe o valor total do café
-        System.out.println("Seu pedido foi confirmado! \n Total a pagar: R$ " + valorCafe);
+        // Exbe o valor total do pedido
+        System.out.println(
+                "Seu pedido foi finalizado.\nTotal a pagar por " + quantidadeCafe + " café(s) é: R$" + valorTotal);
         scanner.close();
     }
 }
