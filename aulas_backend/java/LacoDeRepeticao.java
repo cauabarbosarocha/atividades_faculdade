ddb1e0a55ad96eb0d4e6e88044eb8c2f82366eea
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-10-25 13:21:42 -0300

    Estudo sobre laços de repetições

---
+++ aulas_backend/java/LacoDeRepeticao.java
@@ -0,0 +1,12 @@
+package aulas_backend.java;
+
+public class LacoDeRepeticao {
+    public static void main(String[] args) {
+        // Exemplo de while
+        int numeroWhile = 1;
+        while (numeroWhile <= 10) {
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+    }
+}
