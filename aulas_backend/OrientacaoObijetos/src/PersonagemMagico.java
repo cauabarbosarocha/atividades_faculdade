230b6b86d9decf02ec75474808dab1b2e331e7d2
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-11-11 16:03:43 -0300

    Criação do menu e cadastramento de personagem

---
+++ aulas_backend/OrientacaoObijetos/src/PersonagemMagico.java
@@ -0,0 +1,13 @@
+public class PersonagemMagico {
+    // Atributos
+    String nome;
+    String poderMagico;
+    int nivelMana;
+
+    // Construtor
+    public PersonagemMagico(String nome, String poderMagico, int nivelMana) {
+        this.nome = nome;
+        this.poderMagico = poderMagico;
+        this.nivelMana = nivelMana;
+    }
+}
