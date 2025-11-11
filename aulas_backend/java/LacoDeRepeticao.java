c36c108cadbf8da6d7811694aca7b2dcc8419d90
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-11-11 09:49:05 -0300

    Estudo sobre do-while

--- aulas_backend/java/LacoDeRepeticao.java
+++ aulas_backend/java/LacoDeRepeticao.java
@@ -5,8 +5,14 @@ public class LacoDeRepeticao {
         // Exemplo de while
         int numeroWhile = 1;
         while (numeroWhile <= 10) {
-            System.out.println(numeroWhile);
+            System.out.println("Contagem do while: " + numeroWhile);
             numeroWhile++;
         }
+        // Exwmplo de do wuhile
+        int numeroDoWhile = 1;
+        do {
+            System.out.println("Contagem do do-while: " + numeroDoWhile);
+            numeroDoWhile++;
+        } while (numeroDoWhile <= 10);
     }
 }
