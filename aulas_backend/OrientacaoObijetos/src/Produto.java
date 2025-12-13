def481eecb8d35b484456cccfb8cfc48a2010a23
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-12-13 13:49:57 -0300

    Inicio de estudo sobre herança e polimorfismo

---
+++ aulas_backend/OrientacaoObijetos/src/Produto.java
@@ -0,0 +1,29 @@
+public class Produto {
+    private String nome;
+    private String ingredientes;
+    private double preco;
+
+    // Contrutor padrão
+    public Produto(){
+
+    }
+
+    // Contrutor com parâmetros
+    public Produto(String nome, String ingredientes, double preco) {
+        this.nome = nome;
+        this.ingredientes = ingredientes;
+        this.preco = preco;
+    }
+
+    // Setters e Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public s
+
+}
